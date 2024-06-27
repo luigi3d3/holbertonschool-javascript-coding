b8df46c0e952859704414e4bf53b5d36dcaed09d
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-27 13:16:33 -0500

    Reading a file asynchronously with Node JS

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        const lines = data.trim().split('\n');
+
+        let csCount = 0;
+        let sweCount = 0;
+        const csList = [];
+        const sweList = [];
+
+        for (const line of lines) {
+          const fields = line.split(',');
+          const [firstName, lastName, age, field] = fields;
+
+          if (firstName && lastName && age && field) {
+            if (field === 'CS') {
+              csCount += 1;
+              csList.push(firstName);
+            } else if (field === 'SWE') {
+              sweCount += 1;
+              sweList.push(firstName);
+            }
+          }
+        }
+
+        console.log(`Number of students: ${csCount + sweCount}`);
+        console.log(`Number of students in CS: ${csCount}. List: ${csList.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweCount}. List: ${sweList.join(', ')}`);
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;
