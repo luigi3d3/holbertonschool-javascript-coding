8ffd3a7a469d4539168b6e7ee3eccae4eec28ad5
luigi3d3 <90977697+luigi3d3@users.noreply.github.com>
2024-06-28 10:42:02 -0500

    getup code

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
-#!/usr/bin/node
 const fs = require('fs');
+const filePath = process.argv[2];
 
-fs.readFile(process.argv[2], function (err, data) {
-  if (!err) {
-    console.log(data.toString('utf-8'));
-  } else {
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
     console.log(err);
+  } else {
+    console.log(data);
   }
 });
